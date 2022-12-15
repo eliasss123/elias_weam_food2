c1f10cb9596ed538c879005371d2946f22bf96b8
Ahad Khan <babaa336@gmail.com>
2022-12-16 04:16:27 +0500

    Driver module fixing....

--- lib/view/screens/driver_app/driver_app_home/delivery_detail_page.dart
+++ lib/view/screens/driver_app/driver_app_home/delivery_detail_page.dart
@@ -58,7 +58,14 @@ class DeliveryDetailPage extends StatelessWidget {
                         buttonText: 'Restaurant details',
                         textSize: 12,
                         fontWeight: FontWeight.w500,
-                        onTap: () {},
+                        onTap: () {
+                          showDialog(
+                            context: context,
+                            builder: (_) {
+                              return RestaurantDetailDialog();
+                            },
+                          );
+                        },
                       ),
                     ),
                     CommonImageView(
@@ -183,13 +190,257 @@ class DeliveryDetailPage extends StatelessWidget {
             top: 60,
             left: 25,
             right: 25,
-            child: DropOff(
-              address: '731 S. High Point Street',
-              time: '4 min',
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                DropOff(
+                  address: '731 S. High Point Street',
+                  time: '4 min',
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 146,
+                      child: MyButton(
+                        height: 46,
+                        textSize: 17,
+                        radius: 8.0,
+                        buttonText: 'Order ready',
+                        onTap: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class RestaurantDetailDialog extends StatelessWidget {
+  const RestaurantDetailDialog({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: Get.height * 0.7,
+          child: Card(
+            margin: EdgeInsets.all(20),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 30,
+                vertical: 15,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                        onTap: () => Get.back(),
+                        child: Image.asset(
+                          Assets.imagesX,
+                          height: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  MyText(
+                    text: 'Restaurant details',
+                    size: 23,
+                    weight: FontWeight.w700,
+                    align: TextAlign.center,
+                  ),
+                  MyText(
+                    text: '302 Manor St. Brooklyn',
+                    size: 12,
+                    color: kSecondaryColor,
+                    align: TextAlign.center,
+                    paddingBottom: 8,
+                    paddingTop: 8,
+                  ),
+                  MyText(
+                    text: 'Italia Trattoria  -  Order #701',
+                    size: 14,
+                    weight: FontWeight.w500,
+                    align: TextAlign.center,
+                    paddingBottom: 40,
+                  ),
+                  MyText(
+                    text: 'Notes from the resturant',
+                    size: 14,
+                    weight: FontWeight.w500,
+                    paddingBottom: 13,
+                    paddingLeft: 3,
+                  ),
+                  Container(
+                    height: 113,
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: kSeoulColor1,
+                    ),
+                    child: MyText(
+                      text:
+                          'Please don’t enter the resturant, wait next to the entrance door.',
+                      size: 14,
+                      color: kGreyColor9,
+                    ),
+                  ),
+                  Spacer(),
+                  MyButton(
+                    radius: 13.0,
+                    textSize: 13,
+                    buttonText: 'Contact Support',
+                    onTap: () {
+                      Get.back();
+                      showDialog(
+                        context: context,
+                        builder: (_) {
+                          return AddressDetailsDialog();
+                        },
+                      );
+                    },
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class AddressDetailsDialog extends StatelessWidget {
+  const AddressDetailsDialog({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          height: Get.height * 0.7,
+          child: Card(
+            margin: EdgeInsets.all(20),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 30,
+                vertical: 15,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      GestureDetector(
+                        onTap: () => Get.back(),
+                        child: Image.asset(
+                          Assets.imagesX,
+                          height: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  MyText(
+                    text: 'Address details',
+                    size: 23,
+                    weight: FontWeight.w700,
+                    align: TextAlign.center,
+                  ),
+                  MyText(
+                    text: '731 S. High Point Street',
+                    size: 14,
+                    weight: FontWeight.w700,
+                    color: kSecondaryColor,
+                    align: TextAlign.center,
+                    paddingTop: 8,
+                    paddingBottom: 35.0,
+                  ),
+                  Row(
+                    children: [
+                      MyText(
+                        text: 'Private house',
+                        size: 14,
+                        weight: FontWeight.w700,
+                        paddingRight: 10,
+                      ),
+                      Image.asset(
+                        Assets.imagesCheckRounded,
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                  MyText(
+                    paddingTop: 35,
+                    text: 'Notes from the restaurant',
+                    size: 14,
+                    weight: FontWeight.w500,
+                    paddingBottom: 13,
+                    paddingLeft: 3,
+                  ),
+                  Container(
+                    height: 113,
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: kSeoulColor1,
+                    ),
+                    child: MyText(
+                      text: 'Please don\'t call, leave the order at the door.',
+                      size: 14,
+                      color: kGreyColor9,
+                    ),
+                  ),
+                  Spacer(),
+                  MyButton(
+                    radius: 13.0,
+                    textSize: 13,
+                    buttonText: 'Contact Support',
+                    onTap: () {},
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
